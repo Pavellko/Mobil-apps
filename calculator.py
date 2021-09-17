e9b1b1e09f7f8c4e9e0f7958f4bd418dd19b39b2
Pavellko <pavelkollegov@gmail.com>
2021-09-17 10:16:17 +0300

    v7

--- calculator.py
+++ calculator.py
@@ -2,13 +2,18 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
+from kivy.uix.label import Label
 
 class MainApp(App):
 
     def build(self):
         main_layout = BoxLayout(orientation = "vertical", padding=10, spacing=10)
         self.solution = TextInput(multiline=False, readonly=False, halign="right", font_size=55)
+        self.solution2 = Label(text="Calculator Algoritmica", font_size=55)
+        main_layout.add_widget(self.solution2)
         main_layout.add_widget(self.solution)
+
+        
         buttons = [
             ["7", "8", "9", "/"],
             ["4", "5", "6", "*"],
